5109c604f2aad9b60ac8df43552f060af8ba10e0
Roadcrosser <roadcrosser0@gmail.com>
2021-11-18 18:04:57 +0800

    Fix data add + add package name to build script

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 pyinstaller -F ^
+    --name "Twitch Poll Viewer"
     --add-data "templates;templates" ^
     --add-data "static;static" ^
-    --add-data "config.sample.yaml;config.sample.yaml" ^
+    --add-data "config.sample.yaml;." ^
     .\main.py
 @echo off
 explorer.exe dist
